3c3d472967f20bdd53a343f954ac7fee2e9b80c3
Raven Universe Admin <admin@ravenuniverse.com>
2026-01-15 00:25:40 +0000

    Fix Mailer

--- game/Mailer.js
+++ game/Mailer.js
@@ -3,10 +3,15 @@ const nodemailer = require('nodemailer');
 class Mailer {
     constructor() {
         this.transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // true for 465, false for other ports
             auth: {
-                user: process.env.EMAIL_USER, // ravenuniverseprotocol@gmail.com
-                pass: process.env.EMAIL_PASS  // Application Password
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
     }
